147fb1e2af35e5e28fec96408cfbb4167eb3b299
louis-baranger <louisbaranger@gmail.com>
2022-12-22 10:40:07 +0100

    Second commit: Encryption and post request

--- Client_helper.py
+++ Client_helper.py
@@ -1,8 +1,19 @@
+from os.path import dirname
 from os.path import exists
+from os import urandom
+
+from pathlib import Path
+from base64 import urlsafe_b64encode
+import shutil
+import requests
+
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import shutil
+
+# TODO: Change url to server URL
+# TODO: Might make that a setting in the terminal interface
+url = "http://localhost:8000"
 
 
 def send():
@@ -28,23 +39,53 @@ def send():
 
 
 def send_file(path):
-    if not(exists(path)):
+    p = Path(path)
+    if not (p.is_file()):
         print("Invalid path.")
         return
     print("Please, input your password:")
-    password = input()
-    path_encrypted = encrypt_file(path)
+    password = input().encode('ascii')
 
+    directory = dirname(path)
+    # TODO: This will not work with multiple file extensions e.g. ".tar.gz"
+    path_copy = directory + '/' + p.stem + "_copy" + p.suffix
+
+    i = 0
+    while exists(path_copy):
+        path_copy = directory + '/' + p.stem + "_copy_" + str(i) + p.suffix
+        i += 1
+
+    # TODO: determine what to do with that salt
+    # TODO: more precisely, determine where to store it
+    # TODO: needed for decryption
+    # TODO: is it okay to store it with encrypted file on server?
+    salt = encrypt_file(path, path_copy, password)
+
+    file = {'file': open(path_copy, 'rb')}
+    values = {'extension': p.suffix, 'salt': salt}
+    r = requests.post(url, data=values, files=file)
     return
 
 
-def encrypt_file(path):
+def encrypt_file(path, path_copy, password):
     print("TODO: Implement encrypt_file")
-    encrypted_file = "temp"
-    # create copy, encrypt
+    # Creates a key derivation function using values suggested by the cryptography library
+    salt = urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
 
-    # return path to encrypted file
-    return encrypted_file
+    key = urlsafe_b64encode(kdf.derive(password))
+
+    fernet = Fernet(key)
+    original = open(path, 'rb').read()
+    encrypted = fernet.encrypt(original)
+    encrypted_copy = open(path_copy, 'wb')
+    encrypted_copy.write(encrypted)
+    return salt
 
 
 def send_string():
